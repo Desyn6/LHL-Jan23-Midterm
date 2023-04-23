d090f3961d97b3ce111413612fa4e7a67b0b9ead
Mike <mike.simnes@gmail.com>
2023-04-23 07:59:02 -0700

    add dummy seed data to listings

---
+++ db/seeds/02_listings.sql
@@ -0,0 +1,23 @@
+-- Insert 20 listings
+INSERT INTO listings(title, long_description, condition, thumbnail_url, owner_id, asking_price, sold, deleted)
+VALUES
+('Red Bicycle', 'This is a high-quality bicycle in excellent condition. Perfect for casual riders or commuters!', 'Excellent', 'https://source.unsplash.com/300x300/?bicycle', 1, 250, false, false),
+('Antique Desk', 'Beautifully crafted antique wooden desk with plenty of storage space. Comes with a matching chair!', 'Good', 'https://source.unsplash.com/300x300/?desk', 2, 800, false, false),
+('Vintage Vinyl Records', 'Large collection of classic rock and roll vinyl records. All in excellent condition!', 'Excellent', 'https://source.unsplash.com/300x300/?vinyl,records', 3, 500, false, false),
+('Green Leather Couch', 'Comfortable and stylish green leather couch in great condition. Seats 3 comfortably!', 'Great', 'https://source.unsplash.com/300x300/?couch,leather', 4, 1200, false, false),
+('Black Leather Jacket', 'This high-quality leather jacket is perfect for any occasion. Looks great with jeans or formal wear!', 'Excellent', 'https://source.unsplash.com/300x300/?leather,jacket', 5, 150, false, false),
+('Blue Bicycle', 'A high-quality bicycle in excellent condition. Perfect for casual riders or commuters!', 'Good', 'https://source.unsplash.com/300x300/?bicycle', 6, 300, false, false),
+('Red Dress', 'Stylish red dress with a flattering cut. Perfect for formal occasions or a night out on the town!', 'Excellent', 'https://source.unsplash.com/300x300/?dress', 7, 75, false, false),
+('Antique Vase', 'Beautiful antique vase in great condition. Perfect for displaying fresh flowers or as a decorative piece!', 'Good', 'https://source.unsplash.com/300x300/?vase', 8, 200, false, false),
+('Leather Briefcase', 'This stylish leather briefcase is perfect for professionals on the go. Plenty of storage space for documents and electronics!', 'Great', 'https://source.unsplash.com/300x300/?briefcase,leather', 9, 175, false, false),
+('Black T-Shirt', 'This simple black t-shirt is a staple for any wardrobe. Made from high-quality materials for long-lasting wear!', 'Good', 'https://source.unsplash.com/300x300/?shirt', 10, 20, false, false),
+('Wooden Bookshelf', 'A sturdy and stylish wooden bookshelf in excellent condition. Perfect for displaying your favorite books and decorative items!', 'Excellent', 'https://source.unsplash.com/300x300/?bookshelf', 1, 150, false, false),
+('Vintage Typewriter', 'This vintage typewriter is in excellent working condition and comes with a case for easy transportation. Perfect for writers or collectors!', 'Good', 'https://source.unsplash.com/300x300/?typewriter', 2, 400, false, false),
+('Diamond Engagement Ring', 'This stunning diamond engagement ring is sure to impress. High-quality materials and exquisite craftsmanship make it a truly special piece!', 'Excellent', 'https://source.unsplash.com/300x300/?diamond,ring', 3, 5000, false, false),
+('Brown Leather Armchair', 'This comfortable brown leather armchair is the perfect addition to any living room or study. Great condition and built to last!', 'Great', 'https://source.unsplash.com/300x300/?armchair,leather', 4, 800, false, false),
+('Handmade Pottery Set', 'This beautiful handmade pottery set includes plates, bowls, and mugs. Perfect for everyday use or special occasions!', 'Good', 'https://source.unsplash.com/300x300/?pottery', 5, 250, false, false),
+('Vintage Camera', 'This vintage camera is in excellent working condition and comes with a leather carrying case. Perfect for photographers or collectors!', 'Good', 'https://source.unsplash.com/300x300/?camera', 6, 350, false, false),
+('Blue High Heels', 'These stylish blue high heels are sure to turn heads. Comfortable and in excellent condition!', 'Excellent', 'https://source.unsplash.com/300x300/?high,heels', 7, 50, false, false),
+('Antique Pocket Watch', 'This beautiful antique pocket watch is in great condition and still keeps accurate time. A true collector''s item!', 'Great', 'https://source.unsplash.com/300x300/?pocket,watch', 8, 600, false, false),
+('Leather Messenger Bag', 'This stylish leather messenger bag is perfect for professionals on the go. Plenty of storage space for documents and electronics!', 'Good', 'https://source.unsplash.com/300x300/?messenger,bag', 9, 125, false, false),
+('Vintage Band T-Shirt', 'This vintage band t-shirt is a must-have for any music lover. Made from high-quality materials for long-lasting wear!', 'Good', 'https://source.unsplash.com/300x300/?tshirt,music', 10, 30, false, false);
