89b1e1b8a11cc46d735b48c03eaa78efbd753c4a
Mike <mike.simnes@gmail.com>
2023-04-24 18:22:41 -0700

    clean up code

--- db/inserts/listings.js
+++ db/inserts/listings.js
@@ -6,14 +6,14 @@ const db = require('../connection');
  * @param {{ title: string, description: string, condition: string, asking_price: number, thumbnail_url: string, photos: string[] }} listing - The data for the new listing.
  * @returns {Promise<object>} The newly created listing object, or an error.
  */
-const createListing = function(listing) {
+const createListing = (listing) => {
   const values = [
-      listing.title,
-      listing.description,
-      listing.condition, 
-      listing.asking_price,
-      listing.thumbnail_url,
-      listing.photos
+    listing.title,
+    listing.description,
+    listing.condition,
+    listing.asking_price,
+    listing.thumbnail_url,
+    listing.photos
   ];
   const queryString = `
     INSERT INTO listings(
@@ -22,13 +22,13 @@ const createListing = function(listing) {
       condition,
       asking_price,
       thumbnail_url,
-      photos) 
+      photos)
     VALUES($1, $2, $3, $4, $5, $6)
     `;
   return db.query(queryString, values)
     .then((result) => result.rows[0])
     .catch((err) => console.log(err.message));
-  };
+};
 
 /**
  * Updates the asking price of a listing in the database with the given id.
@@ -37,15 +37,15 @@ const createListing = function(listing) {
  * @returns {Promise<object>} A promise that resolves with the updated listing object, or an error.
  */
 const updateListingPrice = (newPrice, id) => {
-  const values = [newPrice, id]; 
+  const values = [newPrice, id];
   const queryString = `
       UPDATE listings
       SET asking_price = $1
       WHERE id = $2;
   `;
   return db.query(queryString, values)
-      .then((result) => result.rows[0])
-      .catch((err) => console.log(err.message));
+    .then((result) => result.rows[0])
+    .catch((err) => console.log(err.message));
 };
 
 /**
@@ -69,20 +69,21 @@ const soldListing = (id) => {
  * @param {number} id - The ID of the listing to mark as deleted.
  * @returns {Promise<object>} The updated listing object, or an error.
  */
-const deleteListing = (id) => {
+const removeListing = (id) => {
+  const values = [id];
   const queryString = `
     UPDATE listings
     SET delete = true
-    WHERE id = ${id};
+    WHERE id = $1;
   `;
-  return db.query(queryString)
-  .then((result) => result.rows[0])
-  .catch((err) => err.message);
+  return db.query(queryString, values)
+    .then((result) => result.rows[0])
+    .catch((err) => err.message);
 };
 
 module.exports = {
   createListing,
   updateListingPrice,
   soldListing,
-  deleteListing
+  removeListing
 };
