8592277f38f37aa497c1734967289189a5d705fa
Mike <mike.simnes@gmail.com>
2023-04-28 16:46:53 -0700

    add req info to res for navbar render

--- routes/register.js
+++ routes/register.js
@@ -5,7 +5,8 @@ const bcrypt = require('bcrypt');
 const generalQueries = require('../db/queries/general');
 
 router.get('/', (req, res) => {
-  res.render('register');
+  const templateVars = { user: req.session.userInfo };
+  res.render('register', templateVars);
 });
 
 //the following rout creates a new user, adds the info to the database and saves encrypted user's email as a cookie
@@ -15,32 +16,32 @@ router.post("/", (req, res) => {
 
   //check for empty fields
   if (!newUser.Name || !newUser.email || !newUser.password || !newUser['phone-number'] || !newUser.city || !newUser['confirm-password']) {
-    return res.send ({error: 'empty field(s)'})
+    return res.send({error: 'empty field(s)'});
   }
 
   //check if both password fields match, then hash user's password
-  if (newUser.password !== newUser['confirm-password']){
-    return res.send ({error: 'passwords not matching'})
+  if (newUser.password !== newUser['confirm-password']) {
+    return res.send({error: 'passwords not matching'});
   }
-  newUser.password = hashedPassword
+  newUser.password = hashedPassword;
 
-    generalQueries
-      //tries to search for a user that is attempting to register
-      .getUserByEmail(newUser.email)
-      .then(checkUser => {
-        //check if user exists
-        if(checkUser){
-          return res.send ({error: 'user already registered'})
-        }
-        //if user does not exist add user to the db, add a cookie with user's encrypted email then redirect to home page
-        generalQueries
-          .addUser(newUser)
-          req.session.userInfo = newUser.email
-          return res.redirect('/');
-        })
-        .catch((error) => {
-          return res.send (error.message)
-      })
+  generalQueries
+  //tries to search for a user that is attempting to register
+    .getUserByEmail(newUser.email)
+    .then(checkUser => {
+      //check if user exists
+      if (checkUser) {
+        return res.send({error: 'user already registered'});
+      }
+      //if user does not exist add user to the db, add a cookie with user's encrypted email then redirect to home page
+      generalQueries
+        .addUser(newUser);
+      req.session.userInfo = newUser.email;
+      return res.redirect('/');
+    })
+    .catch((error) => {
+      return res.send(error.message);
+    });
 });
 
 module.exports = router;
