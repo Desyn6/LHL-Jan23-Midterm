98e86cfd3a879bc1268770236456f8d8c3d95bc4
Mike <mike.simnes@gmail.com>
2023-04-29 16:53:01 -0700

    fix route for partials

--- routes/mailbox.js
+++ routes/mailbox.js
@@ -4,15 +4,15 @@ const router  = express.Router();
 
 
 router.get('/', (req, res) => {
+  let email = req.session['userInfo']
+  console.log("req.session----", req.session)
   const templateVars = { user: req.session.userInfo };
   res.render('mailbox', templateVars);
-  let email = req.session['userInfo']
-  
   //checking if user is loged
   if(!email){
     return res.redirect('/login')
-  } 
-  res.render('mailbox');
+  }
+  // res.render('mailbox');
 });
 
 module.exports = router;
