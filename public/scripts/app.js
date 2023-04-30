cb211ff3743fdcf607a08db6c730294e873cc9ec
Denny <Dennyng1989@hotmail.com>
2023-04-30 15:02:09 -0700

    Impl get listing ID from url function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,6 @@
 // Client facing scripts here
+// Returns search value for given search parameter
+const getParam = (param) => {
+  const params = new URL(window.location).searchParams;
+  return params.get(param);
+}
