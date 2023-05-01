97ec4ceaa5a08caccfbc65fe66a89bb907e99293
Mike <mike.simnes@gmail.com>
2023-05-01 13:19:32 -0700

    add listing_id to templateVars

--- routes/mailbox.js
+++ routes/mailbox.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  let email = req.session['userInfo']
-  //Check if user is logged in
-  if(!email) return res.redirect('/login');
-
-  const templateVars = { user: req.session.userInfo };
+  let email = req.session['userInfo'];
+  //checking if user is logged
+  if (!email) {
+    return res.redirect('/login');
+  }
+  // pass email and listing_id to mailbox.ejs
+  const templateVars = { user: req.session.userInfo, listing_id: req.query.lid};
   res.render('mailbox', templateVars);
+
 });
 
 module.exports = router;
