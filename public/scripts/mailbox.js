1ea01516f0be9edade4ad0dac587ca392de45ce0
Denny <Dennyng1989@hotmail.com>
2023-05-02 23:18:00 -0700

    Impl inbox item listeners

--- public/scripts/mailbox.js
+++ public/scripts/mailbox.js
@@ -1,5 +1,27 @@
 // Get listing id from url search string
-const listingId = getParam('lid');
+const urlListingId = getParam('lid');
 
-  $.get('/api/mailbox/', {listingId})
-  .then((data) => renderMessages(data));
+// send get request to render thread if redirected with lid 
+if(urlListingId) {
+  $.get('/api/mailbox/', {urlListingId})
+    .then((data) => renderMessages(data));
+}
+
+$(document).ready(function() {
+  //listeners to send get request to mailbox
+  //from-buyer listeners
+  $('.buyer-query').on('click', function() {
+    const $buyerEmail = $(this).children().children('.buyer-email').text();
+    const $listingId = $(this).children().children('.listing-id').text();
+
+    console.log($buyerEmail, $listingId)
+  });
+
+  //to-seller listeners
+  $('.to-seller').on('click', function() {
+    const $sellerEmail = $(this).children().children('.seller-email').text();
+    const $listingId = $(this).children().children('.listing-id').text();
+
+    console.log($sellerEmail, $listingId)
+  });
+})
